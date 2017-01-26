40dfd8526977642534587b66531da9032f2fee54
Detrick <detrick@merzhaus.org>
2017-01-26 23:01:34 +0000

    fixed regex parsing

--- FDLS.pl
+++ FDLS.pl
@@ -33,16 +33,30 @@ sub handle_connection {
 	my $output = shift || $socket;
 	my $exit = 0;
 	my $peeraddress = $socket->peerhost;
+	my $uuid = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
+	my $epoch = "[0-9]{1,10}";
+	my $clientid = "(A-[0-9]{1,3}|HLFDS)";
+	my $band = "(160M|80M|40M|20M|15M|10M|6M|2M|1\.25M|70CM|33CM|23CM)";
+	my $mode = "(PHONE|CW|DIGITAL)";
+	my $callsign = "[A-Z0-9/]*";
+	my $class = "[0-9]{1}[A-Z]{1}";
+	my $section = "[A-Z]{2,3}";
+	my $operator = "[A-Za-z]*";
 	do {
-	        $socket->recv($data,1024);
-	        print "Received from client $peeraddress : $data\n";
+	        $socket->recv($data,2048);
+#	        print "Received from client $peeraddress : $data\n\n";
 
 	        if ($data eq "SENDALLCONTACTS") {
-	                print "Client $peeraddress wants us to send it all the contacts the server has seen\n";
+	                print "Client $peeraddress said $data\n\n";
+	        }
+	        elsif ($data =~ /^$uuid\;$epoch\;$clientid\;$band\;$mode\;$callsign\;$class\;$section\;$operator\;#$/) {
+	                print "Client $peeraddress sent us a log entry, do something with it\n";
+			print "Log data : $data\n\n";
 	        }
 	        elsif ($data) {
-	                print "Client $peeraddress sent us a log entry, do sometihng with it\n";
-	        }
+			print "Client $peeraddress sent us something but it doesn't look like a single log entry\n";
+			print "Log data : $data\n\n";
+		}
 	        else {
 	                print "Client $peeraddress exited\n";
 	        }
