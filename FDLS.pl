06e488b04e32f10550e9937e12302e9627e53dac
Detrick <detrick@merzhaus.org>
2017-01-28 00:54:29 +0000

    added functionality to write logs to a mysql db

--- FDLS.pl
+++ FDLS.pl
@@ -5,16 +5,61 @@ use strict;
 use warnings;
 use threads;
 use IO::Socket::INET;
+use DBI;
+use Data::Dumper qw(Dumper);
 
 # flush after every write
 $| = 1;
 
+# set up our variables
 my $data;
+my $dbh;
 my ($listen,$client_socket);
 my ($peeraddress,$peerport);
 my $ip = '192.168.100.202';
 my $port = '7373';
 my $proto = 'tcp';
+
+
+####################################
+# stuff for later
+####################################
+
+# INSERT some data into 'foo'. We are using $dbh->quote() for
+# quoting the name.
+#$dbh->do("INSERT INTO logs VALUES (1, " . $dbh->quote("Tim") . ")");
+
+# same thing, but using placeholders (recommended!)
+#$dbh->do("INSERT INTO logs VALUES (?, ?)", undef, 2, "Jochen");
+
+# now retrieve data from the table.
+#my $sth = $dbh->prepare("SELECT * FROM logs");
+#$sth->execute();
+#while (my $ref = $sth->fetchrow_hashref()) {
+#	print "Found a row: id = $ref->{'id'}, name = $ref->{'name'}\n";
+#}
+#$sth->finish();
+
+####################################
+sub checklog {
+	my $sth;
+	my @details = split /;/, $data;
+	print "uuid: $details[0]\n";
+	$sth = $dbh->prepare("SELECT idx FROM log WHERE uuid=?");
+	$sth->execute($details[0]);
+	if (my @row = $sth->fetchrow_array) {
+		print "Update log entry\n";
+		$sth = $dbh->prepare("UPDATE log SET epoch=?, clientid=?, band=?, mode=?, callsign=?, class=?, section=?, op=? WHERE uuid=?");
+		$sth->execute($details[1],$details[2],$details[3],$details[4],$details[5],$details[6],$details[7],$details[8],$details[0]) or die;
+	}
+	else {
+		print "Add log entry\n";
+		$sth = $dbh->prepare("INSERT INTO log(uuid,epoch,clientid,band,mode,callsign,class,section,op) VALUES (?,?,?,?,?,?,?,?,?)");
+		$sth->execute($details[0],$details[1],$details[2],$details[3],$details[4],$details[5],$details[6],$details[7],$details[8]) or die;
+	}
+
+}
+####################################
 
 # creating object interface of IO::Socket::INET modules which internally does 
 # socket creation, binding and listening at the specified port address.
@@ -29,6 +74,11 @@ $listen = new IO::Socket::INET (
 print "Waiting for client connection on port $port\n";
 
 sub handle_connection {
+	# talk to the database
+	$dbh = DBI->connect("DBI:mysql:database=FDLS;host=localhost",
+		"root", "fieldday",
+		{'RaiseError' => 1});
+
 	my $socket = shift;
 	my $output = shift || $socket;
 	my $exit = 0;
@@ -47,14 +97,25 @@ sub handle_connection {
 #	        print "Received from client $peeraddress : $data\n\n";
 
 	        if ($data eq "SENDALLCONTACTS") {
-	                print "Client $peeraddress said $data\n\n";
+	                print "Client $peeraddress wants us to send all log entries\n\n";
 	        }
 	        elsif ($data =~ /^$uuid\;$epoch\;$clientid\;$band\;$mode\;$callsign\;$class\;$section\;$operator\;#$/) {
-	                print "Client $peeraddress sent us a log entry, do something with it\n";
+	                print "Client $peeraddress sent us a log entry\n";
+			print "- Check to see if it is already in the database or not, if yes update the record\n";
+			if (!checklog($data)) {
+				# add log entry to db
+			}
+			else {
+				# update the log entry
+			}
+			print "- Add it to the database\n";
+			print "- Send the log entry out to any other clients\n";
 			print "Log data : $data\n\n";
 	        }
 	        elsif ($data) {
 			print "Client $peeraddress sent us something but it doesn't look like a single log entry\n";
+			print "It's probably a bulk upload of log entries. Do we want to process those, or just\n";
+			print "presume we know about them already?\n";
 			print "Log data : $data\n\n";
 		}
 	        else {
