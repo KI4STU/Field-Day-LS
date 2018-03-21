5eaf85f9cc0df6b26555ebaadd3ad8b3b19e504b
KI4STU <detrick@merzhaus.org>
2018-03-21 19:10:33 +0000

    fix adif export module

---
+++ www/api/adif.php
@@ -0,0 +1,51 @@
+<?php
+# export log data in ADIF format
+# originally from https://bitbucket.org/pignology/hlfds
+# modified by Detrick Merz (K4IZ) to function with FDLS (https://github.com/KI4STU/Field-Day-LS)
+
+header("Content-type: text/plain");
+header("Content-Disposition: attachment; filename=FDcontacts.adif");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+$ret = array();
+$ret['data'] = array();
+
+# connect to mysql server, database
+$fdls = new mysqli("127.0.0.1", "phpmyadmin", "fieldday", "FDLS");
+if ($fdls->connect_errno) {
+        echo "Failed to connect to MySQL: (" . $mysqli->connect_errono . ") " . $mysqli->connect_error;
+}
+
+# get sql query results
+$results = $fdls->query('SELECT * FROM log');
+
+# create headers at top of CSV file
+$dt = new DateTime("NOW");  
+echo $dt->format('Y-m-d H:i:s') . " GMT+00:00<ProgramID:4>FDLS<eoh>\n";
+#echo "GMT+00:00<ProgramID:4>FDLS<eoh>\n";
+
+# populate CSV file with data from database
+while ($row = $results->fetch_array()) {
+  $entry = array();
+  $epoch = $row['epoch'];
+  $dt = new DateTime("@$epoch");
+  
+  echo "<QSO_DATE:8>".$dt->format('Ymd');
+  echo "<TIME_ON:6>".$dt->format('His');
+  echo "<TIME_OFF:6>".$dt->format('His');
+  echo "<BAND:".strlen($row['band']).">".$row['band'];
+# NOTE: HamLog currently logs modes (phone, ditigal) which are *not*
+# compliant with the ADIF standard! For now we'll dump the data, but
+# your logging software (or LoTW) may not like it
+  echo "<MODE:".strlen($row['mode']).">".$row['mode'];
+  echo "<CALL:".strlen($row['callsign']).">".$row['callsign'];
+  echo "<CLASS:".strlen($row['class']).">".$row['class'];
+  echo "<ARRL_SECT:".strlen($row['section']).">".$row['section'];
+  echo "<OPERATOR:".strlen($row['section']).">".$row['section'];
+# edit the callsign below to match that of your station (usually
+# your club callsign during Field Day). Be sure to change the "5"
+# to the number of characters in your callsign (e.g. K4IZ = 4, KI4STU = 6)
+  echo "<STATION_CALLSIGN:5>w4owl\n";
+}
+?>
